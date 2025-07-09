db34ba7603b067cffaf57515cb00ade9901f9b0a
BAA77 <142021188+BAA77@users.noreply.github.com>
2025-07-09 05:53:52 -0700

    page.jsx file update for tracking

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,20 @@ import Home from './pages/home/home';
 import AboutUs from './pages/aboutUs/aboutUs';
 import Contact from './pages/contact/contact';
 import PageNF from './pages/pageNF/pageNF';
-import ReactGA from 'react-ga4';
 import { useLocation } from 'react-router-dom';
 import { useEffect } from 'react';
 import ReactGA from 'react-ga4';
-
-ReactGA.initialize('G-EN6GC35M65');
-ReactGA.send('pageview');
 
 function App() {
   const location = useLocation();
 
   useEffect(() => {
+    ReactGA.initialize('G-EN6GC35M65');
+    ReactGA.send({ hitType: 'pageview', page: location.pathname });
+  }, []);
+
+  useEffect(() => {
+    // Send page view on route change
     ReactGA.send({ hitType: 'pageview', page: location.pathname });
   }, [location]);
 
